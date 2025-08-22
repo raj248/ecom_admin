8c1947f9490e20c0c146523f6ee40a7f31e81ce4
raj248 <shashankraj007281@gmail.com>
2025-08-22 11:36:46 +0530

    store customization fix

--- src/components/form/selectOption/SelectLanguageTwo.jsx
+++ src/components/form/selectOption/SelectLanguageTwo.jsx
@@ -4,28 +4,29 @@ import useUtilsFunction from "@/hooks/useUtilsFunction";
 const SelectLanguageTwo = ({ handleSelectLanguage, register, language }) => {
   const { languages } = useUtilsFunction();
 
-  // console.log("lang", lang, "languages", languages, "language", language);
-
   return (
-    <>
-      <select
-        name="language"
-        {...register(`language`, {
-          required: `language is required!`,
-        })}
-        onChange={(e) => handleSelectLanguage(e.target.value)}
-        className="block w-20 h-8 border border-emerald-400 bg-gray-100 dark:bg-gray-700 px-2 py-1 text-sm dark:text-gray-300 focus:outline-none rounded-md form-select focus:bg-white dark:focus:bg-gray-700"
-      >
-        <option value={language} defaultChecked hidden>
-          {language}
+    <select
+      name="language"
+      {...register(`language`, {
+        required: `language is required!`,
+      })}
+      onChange={(e) => handleSelectLanguage(e.target.value)}
+      className="block w-28 h-8 border border-emerald-400 bg-gray-100 dark:bg-gray-700 px-2 py-1 text-sm dark:text-gray-300 focus:outline-none rounded-md form-select focus:bg-white dark:focus:bg-gray-700"
+    >
+      {/* Current selected language */}
+      {language && (
+        <option value={language.code} hidden>
+          {language.flag} {language.name}
         </option>
-        {languages?.map((lang) => (
-          <option key={lang?._id} value={lang?.code}>
-            {lang?.code}{" "}
-          </option>
-        ))}
-      </select>
-    </>
+      )}
+
+      {/* Available languages */}
+      {languages?.map((lang) => (
+        <option key={lang._id} value={lang.code}>
+          {lang.flag} {lang.name}
+        </option>
+      ))}
+    </select>
   );
 };
 
